5583845018acdfe28e7fc0af05beb0398ca8f6e6
ericxs33 <ericxy2007@gmail.com>
2026-03-16 11:51:47 -0300

    aula 2

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,29 @@
+print("Hello World")
+
+print(7 + 4)
+
+print("3" + "3") # ESTOU CONCATENANDO STRINGS
+
+'''
+Isso são comentários
+De
+Multiplas
+Linhas
+'''
+
+# VARIAVEIS
+# A variavel nome recebe "Eric" e guarda na memoria
+nome = "Eric"
+Idade = 18
+peso = 70
+
+print(nome)
+print (f"Opa, {nome}")
+
+# INPUT - FORMULARIO SIMPLES NO PROMPT DE COMANDO (CMD)
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+peso = input("Digite seu peso: ")
+
+print(nome, idade, peso)
---
+++ aula02-variaveis-operadores/desafio-aula02.py
@@ -0,0 +1,9 @@
+nome = input("Digite seu nome: ")
+print(f"Bem vindo {nome}")
+
+data = input("Digite a sua data de nascimento: ")
+print(f"Certo, sua data de nascimento é {data}")
+
+soma1 = input("Digite o primeiro número a ser somado: ")
+soma2 = input("Digite o segundo número a ser somado: ")
+print (f"{soma1 +  soma2}")e
